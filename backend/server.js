af17e3048e24bc6bef0b54f20e85a47346f66f9a
wusiyian <2644753556@qq.com>
2026-06-11 14:45:18 +0800

    modified:   backend/server.js 	new file:   frontend/build/asset-manifest.json 	new file:   frontend/build/favicon.ico 	new file:   frontend/build/index.html 	new file:   frontend/build/logo192.png 	new file:   frontend/build/logo512.png 	new file:   frontend/build/manifest.json 	new file:   frontend/build/robots.txt 	new file:   frontend/build/static/css/main.a577660b.css 	new file:   frontend/build/static/css/main.a577660b.css.map 	new file:   frontend/build/static/js/main.d295c0bf.js 	new file:   frontend/build/static/js/main.d295c0bf.js.LICENSE.txt 	new file:   frontend/build/static/js/main.d295c0bf.js.map 	new file:   frontend/build/static/media/background.64888f0bedca38468102.png 	modified:   frontend/package.json 	modified:   package.json

--- backend/server.js
+++ backend/server.js
@@ -6,6 +6,7 @@ const userRoutes = require("./routes/userRoutes");
 const { notFound, errorHandler } = require("./middleware/errorMiddleware");
 const chatRoutes = require("./routes/chatRoutes")
 const messageRoutes = require('./routes/messageRoutes')
+const path = require('path')
 
 dotenv.config()
 connectDB()
@@ -13,13 +14,21 @@ const app = express()
 
 app.use(express.json())//接受json数据
 
-app.get("/", (req, res) => {
-    res.send("API is running")
-})
-
 app.use('/api/user', userRoutes)
 app.use('/api/chat', chatRoutes)
 app.use('/api/message', messageRoutes)
+
+const __dirnamel = path.resolve()
+if (process.env.NODE_ENV = 'production') {
+    app.use(express.static(path.join(__dirnamel, "/frontend/build")))
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirnamel, "frontend", "build", "index.html"))
+    })
+} else {
+    app.get("/", (req, res) => {
+        res.send("API is Running Successfully")
+    })
+}
 
 app.use(notFound)
 app.use(errorHandler)
