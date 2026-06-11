86e4d9419793afa28c87eb96cc3974997a8955a7
wusiyian <2644753556@qq.com>
2026-06-11 17:03:39 +0800

    modified:   backend/server.js

--- backend/server.js
+++ backend/server.js
@@ -19,9 +19,9 @@ app.use('/api/chat', chatRoutes)
 app.use('/api/message', messageRoutes)
 
 const __dirnamel = path.resolve()
-if (process.env.NODE_ENV = 'production') {
+if (process.env.NODE_ENV === 'production') {
     app.use(express.static(path.join(__dirnamel, "/frontend/build")))
-    app.get('*', (req, res) => {
+    app.get(/.*/, (req, res) => {
         res.sendFile(path.resolve(__dirnamel, "frontend", "build", "index.html"))
     })
 } else {
